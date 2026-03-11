31193f25edb934172e49e9e88151f28644211a44
kietn20 <kietnguyen3698@gmail.com>
2026-03-10 21:55:00 -0700

    Initialize MediScribe backend project structure with Maven and Spring Boot

---
+++ mediscribe-backend/src/main/java/com/mediscribe/mediscribe_backend/MediscribeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mediscribe.mediscribe_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediscribeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediscribeBackendApplication.class, args);
+	}
+
+}
---
+++ mediscribe-backend/src/test/java/com/mediscribe/mediscribe_backend/MediscribeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscribe.mediscribe_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscribeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
